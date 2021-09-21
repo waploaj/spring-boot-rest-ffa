886faa37a572e608c1f80d98d10a29ad66e6b0b3
Abdallah Abdillah <waploaj@icloud.com>
2021-09-21 20:52:19 +0300

    Employee datasource

--- src/main/kotlin/com/waploaj/fieldforceapi/datasource/EmployeeDatasource.kt
+++ src/main/kotlin/com/waploaj/fieldforceapi/datasource/EmployeeDatasource.kt
@@ -1,8 +1,11 @@
 package com.waploaj.fieldforceapi.datasource
 
 import com.waploaj.fieldforceapi.model.Person
+import org.springframework.stereotype.Repository
 
+@Repository
 interface EmployeeDatasource {
 
     fun getEmployee():Collection<Person>
+
 }

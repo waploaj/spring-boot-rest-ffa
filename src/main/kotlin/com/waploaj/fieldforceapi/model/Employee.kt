3327b2d1f682532b43a9cbea92fede7e1ef4697f
Abdallah Abdillah <waploaj@icloud.com>
2021-09-21 20:51:19 +0300

    Employee model

--- src/main/kotlin/com/waploaj/fieldforceapi/model/Employee.kt
+++ src/main/kotlin/com/waploaj/fieldforceapi/model/Employee.kt
@@ -1,3 +1,23 @@
 package com.waploaj.fieldforceapi.model
 
-data class Person(val personId:Int, val username:String, val password:String, val deleted:Int, val language:String)
+import org.jetbrains.annotations.NotNull
+import javax.persistence.*
+
+
+@Entity
+ data class  Person(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val personId:Int = 0,
+
+    @NotNull
+    val username:String,
+    @NotNull
+    val password:String,
+    val deleted:Int,
+    val hashcode:Int,
+    val language_code:String,
+    val token:String?,
+    val lastUpdate: String,
+    val language:String,
+)

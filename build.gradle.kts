001f385589efef8ebc8f94bf5001e12027b3c352
Abdallah Abdillah <waploaj@icloud.com>
2021-09-21 20:46:43 +0300

    JPA dependecy

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")

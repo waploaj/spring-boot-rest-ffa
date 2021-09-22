69d31d2e4cf428c66ff477de4d3567ec6a5fe4ce
Abdallah Abdillah <waploaj@icloud.com>
2021-09-23 00:39:26 +0300

    update customer model

--- src/main/kotlin/com/waploaj/fieldforceapi/model/Customer.kt
+++ src/main/kotlin/com/waploaj/fieldforceapi/model/Customer.kt
@@ -1,33 +1,35 @@
 package com.waploaj.fieldforceapi.model
 
-import jdk.jfr.Percentage
+import lombok.AllArgsConstructor
+import lombok.Data
+import lombok.NoArgsConstructor
 import org.jetbrains.annotations.NotNull
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "ospos_customer")
 data class Customer(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val person_id:Int = -1,
+    val person_id: Int = -1,
 
     @NotNull
-    val storeBusinessName:String,
-    val latitude:String,
-    val longitude:String,
-    val retailId:Int,
-    val channelId:Int,
-    val locationId:Int,
-    val cardId:Int,
-    val point:Float,
+    val storeBusinessName: String,
+    val latitude: String,
+    val longitude: String,
+    val retailId: Int,
+    val channelId: Int,
+    val locationId: Int,
+    val cardId: Int,
+    val point: Float,
     val discountPercentage: String,
-    val saleTaxCode:String,
-    val taxable:Int,
-    val deleted:Int,
-    val packageId:Int,
+    val saleTaxCode: String,
+    val taxable: Int,
+    val deleted: Int,
+    val packageId: Int,
 
-
-
-)
+    )

5f063c204767490328efadc9ecb5512c65a1e824
Abdallah Abdillah <waploaj@icloud.com>
2021-09-23 00:53:43 +0300

    update employee model

--- src/main/kotlin/com/waploaj/fieldforceapi/model/Employee.kt
+++ src/main/kotlin/com/waploaj/fieldforceapi/model/Employee.kt
@@ -1,23 +1,27 @@
 package com.waploaj.fieldforceapi.model
 
 import org.jetbrains.annotations.NotNull
-import javax.persistence.*
+import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 
 @Entity
- data class  Person(
+data class Person(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val personId:Int = 0,
+    val personId: Long? = null,
 
     @NotNull
-    val username:String,
+    val username: String,
     @NotNull
-    val password:String,
-    val deleted:Int,
-    val hashcode:Int,
-    val language_code:String,
-    val token:String?,
-    val lastUpdate: String,
-    val language:String,
+    val password: String,
+    val deleted: Int,
+    val hashcode: Int,
+    val language_code: String,
+    val token: String?,
+    val lastUpdate: LocalDate? = LocalDate.now(),
+    val language: String,
 )
